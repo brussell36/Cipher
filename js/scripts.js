633313b79b51942d0e5311f27b6260f582d17e18
Thomas Glenn <thomasgrantglenn@mac.com>
2020-06-01 14:57:19 -0600

    add back end code the returns the first position letter of a prompted phrase.
    co-authored by: benjaminrussell36@gmail.com
    co-authored by:
    co-authored by: joseph pearce

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// This is back-end code
+function userSentence() {
+  const words = prompt("Enter your sentence.");
+  return words.charAt(0);
+    
+}
+
+// let firstLetter = userSentence.charAt(0);
+// // let lastLetter = userSentence.lastIndexOf();
+// console.log(userSentence(firstLetter));
+
+
+
+
+// prompt(words);
+console.log(userSentence());
+
+// this is front-end code
+// $(document).ready(function(){
+//   $(button).click(function);
+//    
+    
+  // });
+
